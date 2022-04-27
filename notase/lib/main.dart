be1eaa41330c985f2ea9918e3eb72a4dbedafb69
julio.maia <julio.maia@oser.app>
2022-04-27 20:00:43 -0300

    mock feito

--- notase/lib/main.dart
+++ notase/lib/main.dart
@@ -1,55 +1,101 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'post_detail.dart';
+
+Future<List<Post>> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts');
+
+  if (response.statusCode == 200) {
+    List<dynamic> body = jsonDecode(response.body);
+
+      List<Post> posts = body
+        .map(
+          (dynamic item) => Post.fromJson(item),
+        )
+        .toList();
+
+      return posts;
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title:  'NotaSE',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'NotaSE'),
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+void main() => runApp(MyApp(post: fetchPost()));
 
-  final String title;
+class MyApp extends StatelessWidget {
+  final Future<List<Post>> post;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+  MyApp({Key key, this.post}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'NotaSE',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: Container(
-        child: ListView(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              'Título',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-            ),
-            Text(
-              'Email',
-              style: TextStyle(color: Colors.grey, fontSize: 14.0),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('NotaSE'),
         ),
-      ),
+
+      body: Container(
+          child: FutureBuilder<List<Post>>(
+            future: post,
+            builder: (context, AsyncSnapshot<List<Post>> snapshot) {
+              if (snapshot.hasData) {
+                List<Post> posts = snapshot.data;
+                return ListView(
+                  children: posts
+                    .map(
+                    (Post post) => ListTile(
+                      title: Text(post.title),
+                      subtitle: Text("${post.userId}"),
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => PostDetail(
+                            post: post,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      )
     );
   }
 }
